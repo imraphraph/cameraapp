3cddec8720332b9a2c1dcadf85ca6a3a90663b64
“Raphael” <raphaellim.creative@gmail.com>
2016-09-09 16:16:54 +0800

    authentication done

--- cameraApp/cameraApp/LoginViewController.swift
+++ cameraApp/cameraApp/LoginViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("dismissKeyboard")))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func dismissKeyboard() {
+    emailTextField.resignFirstResponder()
+    passwordTextField.resignFirstResponder()
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 
+    
+    @IBAction func loginButton(sender: UIButton) {
+        
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
+        return
+        }
+        FIRAuth.auth()?.signInWithEmail(email, password: password) { (user, error) in
+            if let person = user {
+                
+                User.signIn(person.uid)
+                
+                self.performSegueWithIdentifier("HomeSegue", sender: nil)
+
+            }else {
+                let controller = UIAlertController(title: "Registration Failed", message: error?.localizedDescription, preferredStyle: .Alert)
+                let dismissButton = UIAlertAction(title: "Try Again", style: .Default, handler: nil)
+                
+                controller.addAction(dismissButton)
+                
+                self.presentViewController(controller, animated: true, completion: nil)
+            }
+            
+        }
+    }
+    
+    
+    //allows for the backToLogin segue
+    @IBAction func backToLogin (segue: UIStoryboardSegue){
+    
+   
+    }
 
 }
 
